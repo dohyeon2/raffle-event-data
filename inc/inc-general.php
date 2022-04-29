c9c3f18837c6632d869793afaa08e849e074b600
주도현 <judohyeon@judohyeon-ui-MacBookPro.local>
2022-04-29 19:31:48 +0900

    Feat:메인 스크립트 및 리퀘스트 프리서브 커스텀 액션 생성

--- inc/inc-general.php
+++ inc/inc-general.php
@@ -10,8 +10,13 @@ function enqueue_style()
     $screen = get_current_screen()->id;
     $filename = plugins_url("/assets/js/$screen.js", FILE);
     if (file_exists(PLUGIN_PATH . "/assets/js/$screen.js")) {
-        wp_register_script("$screen-script", $filename, null, null, true);
+        wp_register_script("$screen-script", $filename, ["jquery"], null, true);
         wp_enqueue_media();
         wp_enqueue_script("$screen-script");
     }
 }
+
+add_filter('rest_pre_serve_request',function($served, $result, $request, $server){
+    do_action("raffle_event_rest_pre_serve_request", $served, $result, $request, $server);
+    return $served;
+},10,4);
