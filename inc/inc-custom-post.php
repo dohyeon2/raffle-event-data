f6ebf6db39c9a64031af143174f07e9b3e2d7a8c
주도현 <judohyeon@judohyeon-ui-MacBookPro.local>
2022-04-29 19:31:20 +0900

    Feat:커스텀 메뉴 페이지 추가 및 custom-post 폴더 참조

--- inc/inc-custom-post.php
+++ inc/inc-custom-post.php
@@ -11,6 +11,6 @@ function my_custom_menu_page()
     add_menu_page("래플이벤트", "래플이벤트", "manage_options", "raffle");
 }
 
-foreach(glob(dirname(__FILE__)."/custom-post/*.php") as $value){
+foreach (glob(dirname(__FILE__) . "/custom-post/*.php") as $value) {
     include_once $value;
 }
