f4065b80108241e34cdaa3428d56f6ced73a8430
주도현 <judohyeon@judohyeon-ui-MacBookPro.local>
2022-04-29 19:28:57 +0900

    Feat:REST API ATTR추가 구문 추가 및 before request callback 추가

--- class/class-custom-post.php
+++ class/class-custom-post.php
@@ -2,7 +2,7 @@
 
 class raffle_event_custom_post
 {
-    public function __construct($array = [], $callbacks = [])
+    public function __construct($array = [], $callbacks = [], $rest_attr = [])
     {
         $default = [
             "register_options" => [
@@ -13,6 +13,7 @@ class raffle_event_custom_post
                 'public'      => true,
                 'has_archive' => true,
                 'capability_type' => 'post',
+                "show_in_rest" => true,
                 'supports' => [],
             ]
         ];
@@ -26,7 +27,9 @@ class raffle_event_custom_post
             },
             "type_custom_columns" => function () {
             },
+            "before_request_served_callback"=> function(){}
         ];
+        $this->rest_attr = $rest_attr;
         $this->array = array_merge_recursive($default, $array);
         $this->callbacks = array_replace_recursive($defaultCallbacks, $callbacks);
 
@@ -35,6 +38,9 @@ class raffle_event_custom_post
 
         $this->array["register_options"]["labels"]["name"] = $this->array["post_label_name"];
         $this->array["register_options"]["labels"]["singular_name"] = $this->array["post_type_name"];
+        $js_name = $this->array["post_type_name"]. '';
+        $js_name = plugins_url('/assets/js/post_type/' . $js_name . '.js',FILE);
+        $this->js_name = $js_name;
 
         //스크립트 등록
         add_action('init', array($this, "custom_post_type"), 10);
@@ -45,14 +51,66 @@ class raffle_event_custom_post
         //리스트 열 수정
         add_filter('manage_' . $this->array["post_type_name"] . '_posts_columns', array($this, "type_posts_columns"));
         add_action('manage_' . $this->array["post_type_name"] . '_posts_custom_column', array($this, "type_custom_columns"), 10, 2);
+        add_action('rest_api_init', array($this, 'register_rest_attrs'));
+        add_action('raffle_event_rest_pre_serve_request',array($this,'before_request_served'),10,4);
     }
-
+    public function before_request_served($served, $result, $request, $server){
+        $this->callbacks["before_request_served_callback"]($result, $request, $server, $this->array);
+        return $served;
+    }
+    public function register_rest_attrs()
+    {
+        register_rest_field(
+            array($this->array["post_type_name"]),
+            'fimg_url',
+            array(
+                'get_callback'    => function ($object, $field_name, $request) {
+                    if ($object['featured_media']) {
+                        $img = wp_get_attachment_image_src($object['featured_media'], 'app-thumb');
+                        return $img[0];
+                    }
+                    return false;
+                },
+                'update_callback' => null,
+                'schema'          => null,
+            )
+        );
+        register_rest_field($this->array["post_type_name"], 'metadata', array(
+            'get_callback' => function ($data) {
+                $meta = array_filter(get_post_meta($data['id']), function ($key) {
+                    preg_match("/^_.?/", $key, $matches);
+                    if (count($matches) === 0) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }, ARRAY_FILTER_USE_KEY);
+                return array_map(function ($value) {
+                    return $value[0];
+                }, $meta);
+            },
+        ));
+        register_rest_field($this->array["post_type_name"], 'categories_slug', array(
+            'get_callback' => function ($data) {
+                return array_map(function ($value) {
+                    return get_category($value);
+                }, $data["categories"]);
+            },
+        ));
+        register_rest_field($this->array["post_type_name"], 'request_server_time', array(
+            'get_callback' => function ($data) {
+                return time() + 9*60*60;
+            },
+        ));
+        foreach($this->rest_attr as $key => $value){
+            register_rest_field($this->array["post_type_name"], $key, $value);
+        }
+    }
     public function current_screen($curr_screen)
     {
         $post_type_name = $this->array['post_type_name'];
+        $js_name = $this->js_name;
         if ($curr_screen->id === $post_type_name) {
-            $js_name = $post_type_name . '';
-            $js_name = TM_URI . '/assets/js/post_type/' . $js_name . '.js';
             if (file_exists(TM_PATH . '/assets/js/post_type/' . $post_type_name . '.js')) {
                 wp_enqueue_script($js_name, $js_name, array("jquery", "jquery-ui-sortable"), false, true);
             }
